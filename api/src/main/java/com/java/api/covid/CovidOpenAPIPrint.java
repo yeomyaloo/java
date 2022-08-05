1c3286668d53ceddea74cd30856c9ff46643e733
hwiju yeom <yeom456@gmail.com>
2022-08-05 19:29:11 +0900

    8/5
    api 사사용해보자

---
+++ api/src/main/java/com/java/api/covid/CovidOpenAPIPrint.java
@@ -0,0 +1,64 @@
+package com.java.api.covid;
+
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class CovidOpenAPIPrint {
+        public static void main(String[] args) throws IOException {
+
+
+            /**자바를 사용해서 해당 공공데이터를 받아오는 방법**/
+            //url 생성
+            StringBuilder urlBuilder = new StringBuilder("http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson"); /*URL*/
+            urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "="); /*Service Key*/
+            urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
+            urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
+            urlBuilder.append("&" + URLEncoder.encode("startCreateDt","UTF-8") + "=" + URLEncoder.encode("20200310", "UTF-8")); /*검색할 생성일 범위의 시작*/
+            urlBuilder.append("&" + URLEncoder.encode("endCreateDt","UTF-8") + "=" + URLEncoder.encode("20200315", "UTF-8")); /*검색할 생성일 범위의 종료*/
+
+            URL url = new URL(urlBuilder.toString());
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Content-type", "application/json");
+
+
+            System.out.println("Response code: " + conn.getResponseCode());
+
+
+            BufferedReader rd;
+            if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+                rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            } else {
+                rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+            }
+
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = rd.readLine()) != null) {
+                sb.append(line);
+            }
+
+            rd.close();
+            conn.disconnect();
+
+
+            /** 위에는 공공데이터를 받아오는 방법이다.
+             * 받아온 데이터를 JSON으로 파싱해주어야 한다. (XML 형식임)
+             * 또한 필요 목적에 맞게 데이터를 가공하면 된다.
+             * **/
+
+            System.out.println(sb.toString());
+
+
+        }
+    }
+
+
+
+
+
+

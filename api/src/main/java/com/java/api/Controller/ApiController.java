0d3281995c991116f75c9370fd2f9bb3078f8ea5
hwiju yeom <yeom456@gmail.com>
2022-08-07 23:13:23 +0900

    8/7

---
+++ api/src/main/java/com/java/api/Controller/ApiController.java
@@ -0,0 +1,58 @@
+package com.java.api.Controller;
+
+
+import org.springframework.http.HttpRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class ApiController {
+
+    //주소 검색을 위한 팝업페이지를 보여주기 위한 컨트롤러
+    @RequestMapping("/address")
+    public String addresspopup(HttpServletRequest request, Model model){
+        //request.setCharacterEncoding("UTF-8");  //한글깨지면 주석제거
+        //request.setCharacterEncoding("EUC-KR");  //해당시스템의 인코딩타입이 EUC-KR일경우에
+        String inputYn = request.getParameter("inputYn");
+        String roadFullAddr = request.getParameter("roadFullAddr");
+        String roadAddrPart1 = request.getParameter("roadAddrPart1");
+        String roadAddrPart2 = request.getParameter("roadAddrPart2");
+        String engAddr = request.getParameter("engAddr");
+        String jibunAddr = request.getParameter("jibunAddr");
+        String zipNo = request.getParameter("zipNo");
+        String addrDetail = request.getParameter("addrDetail");
+        String admCd    = request.getParameter("admCd");
+        String rnMgtSn = request.getParameter("rnMgtSn");
+        String bdMgtSn  = request.getParameter("bdMgtSn");
+        String detBdNmList  = request.getParameter("detBdNmList");
+        String bdNm  = request.getParameter("bdNm");
+        String bdKdcd  = request.getParameter("bdKdcd");
+        String siNm  = request.getParameter("siNm");
+        String sggNm  = request.getParameter("sggNm");
+        String emdNm  = request.getParameter("emdNm");
+        String liNm  = request.getParameter("liNm");
+        String rn  = request.getParameter("rn");
+        String udrtYn  = request.getParameter("udrtYn");
+        String buldMnnm  = request.getParameter("buldMnnm");
+        String buldSlno  = request.getParameter("buldSlno");
+        String mtYn  = request.getParameter("mtYn");
+        String lnbrMnnm  = request.getParameter("lnbrMnnm");
+        String lnbrSlno  = request.getParameter("lnbrSlno");
+        String emdNo  = request.getParameter("emdNo");
+
+        String confmKey = "devU01TX0FVVEgyMDIyMDgwMjAwMDkxNzExMjgzNzI";
+
+        //발급 받은 키를 넘겨주고
+        model.addAttribute("confmKey", confmKey);
+        model.addAttribute("")
+
+        return "addresspopup";
+    }
+
+
+
+
+}

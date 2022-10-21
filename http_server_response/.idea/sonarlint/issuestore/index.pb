6c09abd9df62d3630c51d98378dd8715aea2147d
yeomhwiju <yeom456@gmail.com>
2022-10-21 23:00:27 +0900

    10/21

--- http_server_response/.idea/sonarlint/issuestore/index.pb
+++
@@ -1,5 +0,0 @@
-
-7
-pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
-g
-7src/main/java/com/java/http/server/response/Server.java,9\6\9695a47b9095d12b012e31f088da029fdcb1a26c
--- http_server_response/src/main/java/com/java/http/server/response/Server.java
+++ http_server_response/src/main/java/com/java/http/server/response/Server.java
@@ -1,36 +1,25 @@
 package com.java.http.server.response;
 
+import com.sun.net.httpserver.HttpServer;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-
+    BufferedReader bufferedReader;
+    String line;
+    StringBuffer stringBuffer;
     public static void main(String[] args) {
+
+
+        
+    }
         int port;
         ServerSocket serverSocket;
         Socket socket;
         BufferedReader input;
 
-        try{
-
-            //해당 main의 cmd 모드로 실행해서 0번째 인덱스에 들어온 인자를 port로 취급..
-            //shttp port... (cmd 명령어)
-            port = Integer.parseInt(args[0]);
-        } catch (NumberFormatException e) {
-            System.out.println("given default port");
-            port = 3000;
-        }
-
-
-        try{
-            socket = serverSocket.accept();
-        } catch (IOException e) {
-
-        }
-
-    }
 
 
 
